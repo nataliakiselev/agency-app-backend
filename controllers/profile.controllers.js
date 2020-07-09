7b919710fb163009ae27d87853e5f52ed6fe057a
Natalia Kiselev <natalia.kiselev1@gmail.com>
2020-07-09 15:10:12 +0100

    cleanup

--- controllers/profile.controllers.js
+++ controllers/profile.controllers.js
@@ -8,14 +8,7 @@ export const addPhotos = (req, res) => {
     if (err) return res.status(400).end();
     if (!result.length) return res.sendStatus(404);
     const profile = result[0];
-    console.log(profile._id);
-    console.log(req.params.id, "id");
 
-    // console.log('photos', profile.photos)
-    console.log("files", req.files);
-
-    // const files = Object.values(req.files);
-    // console.log(files);
     profile.photos = [
       ...profile.photos,
       ...req.files.map((file) => {
